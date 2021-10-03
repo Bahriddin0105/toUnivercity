553bb6e09d0d1727420d666e246fee27112d9fba
Eric <82119304+Yadzim@users.noreply.github.com>
2021-10-03 14:16:21 +0500

    .

--- xakaton/src/components/main-comp/Test.js
+++ xakaton/src/components/main-comp/Test.js
@@ -1,44 +1,68 @@
-import React, { Component } from 'react';
-import {Container, Variant} from './style'
+/** @format */
+
+import React, { Component } from "react";
+import { Container } from "./style";
+import Question from "./question";
+import { Link } from "react-router-dom";
+import { getQuestions } from "../../cerver/face-server";
+
 export default class Test extends Component {
-    state = {
-        
-    }
-    render() {
-        return (
-            <Container>
-                <div className="place">
-                    <div className="scrol">
-                        <div></div>
-                    </div>
-                    <div className="question">
-                        <p>question <span>6</span>/15</p>
-                        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                             Eligendi unde commodi iusto consequatur ducimus deserunt.</h5>
-                    </div>
-                    <hr />
-                    <div className="variants">
-                        <div>
-                            <Variant>
-                                <div className="order">A</div>
-                                <span>Lorem ipsum dolor sit amet.</span>
-                            </Variant>
-                            <Variant>
-                                <div className="order">B</div>
-                                <span>Lorem ipsum dolor sit amet.</span>
-                            </Variant>
-                            <Variant>
-                                <div className="order">C</div>
-                                <span>Lorem ipsum dolor sit amet.</span>
-                            </Variant>
-                            <Variant>
-                                <div className="order">D</div>
-                                <span>Lorem ipsum dolor sit amet.</span>
-                            </Variant>
-                        </div>
-                    </div>
-                </div>
-            </Container>
-        )
-    }
+  state = {
+    tests: [],
+    categorys: [],
+    transform: 0,
+  };
+
+  componentDidMount() {
+    const tests = getQuestions();
+    this.setState({
+      tests,
+    });
+  }
+
+  nextQuestion = () => {
+    let transform = this.state.transform;
+    transform -= 680;
+    document.querySelector(
+      ".slider"
+    ).style.transform = `translateY(${transform}px)`;
+    this.setState({ transform });
+  };
+
+  render() {
+    const { tests } = this.state;
+    // const length = tests.length
+    let order = 1;
+    console.log(tests);
+    return (
+      <Container>
+        <div className='box'>
+          <div className='slider'>
+            {tests.map(({ _id, title, variants }) => (
+              <>
+                {order === 10 ? (
+                  <Link to='/natija'>
+                    <button className='btn btn-primary'>Show result</button>
+                  </Link>
+                ) : (
+                  <Question
+                    key={_id}
+                    askTitle={title}
+                    variants={variants}
+                    order={order++}
+                    onNextQuestion={this.nextQuestion}
+                  />
+                )}
+              </>
+            ))}
+          </div>
+        </div>
+      </Container>
+    );
+  }
 }
+
+// https://github.com/chrisblakely01/quiz-app/blob/master/final/src/App.js#L43
+// https://reactjs.org/docs/hooks-state.html
+// https://codepen.io/Jake_Woods/pen/vYYoYxo
+// https://freefrontend.com/javascript-carousels/
