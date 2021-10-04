7fb2b6929166cea66cbe7680abbbd2f350f4a93c
Bahriddin0105 <87873365+Bahriddin0105@users.noreply.github.com>
2021-10-04 20:23:32 +0500

    .

---
+++ xakaton/src/js/Ptest.js
@@ -0,0 +1,337 @@
+const ptest=[
+  {
+    "id": 3,
+    "category": "BUSINESS",
+    "title": "Work with a team to solve a complex problem."
+  },
+  {
+    "id": 4,
+    "category": "BUSINESS",
+    "title": "Manage a project from start to finish"
+  },
+  {
+    "id": 5,
+    "category": "BUSINESS",
+    "title": "Work on the communications for a company."
+  },
+  {
+    "id": 6,
+    "category": "BUSINESS",
+    "title": "Invest money."
+  },
+  {
+    "id": 7,
+    "category": "BUSINESS",
+    "title": "Increase employee morale by making them feel valued and appreciated."
+  },
+  {
+    "id": 8,
+    "category": "BUSINESS",
+    "title": "Ensure your team goes out of their way to make customers happy."
+  },
+  {
+    "id": 9,
+    "category": "BUSINESS",
+    "title": "Learn all aspects of running a business."
+  },
+  {
+    "id": 10,
+    "category": "BUSINESS",
+    "title": "Work to conserve Earth’s natural resources."
+  },
+  {
+    "id": 11,
+    "category": "BUSINESS",
+    "title": "Learn how global trade works."
+  },
+  {
+    "id": 12,
+    "category": "BUSINESS",
+    "title": "Less time at a desk and more time interacting with other departments and people."
+  },
+  {
+    "id": 13,
+    "category": "BUSINESS",
+    "title": "Lead a team of people."
+  },
+  {
+    "id": 14,
+    "category": "BUSINESS",
+    "title": "Research business trends."
+  },
+  {
+    "id": 15,
+    "category": "BUSINESS",
+    "title": "Analyze the profit and loss of a business."
+  },
+  {
+    "id": 16,
+    "category": "BUSINESS",
+    "title": "Foster an environment where employees can excel and use their strengths."
+  },
+  {
+    "id": 17,
+    "category": "BUSINESS",
+    "title": "Train others to provide exceptional customer service."
+  },
+  {
+    "id": 18,
+    "category": "ART",
+    "title": "Develop websites"
+  },
+  {
+    "id": 19,
+    "category": "ART",
+    "title": "Design graphics."
+  },
+  {
+    "id": 20,
+    "category": "ART",
+    "title": "Study music theory."
+  },
+  {
+    "id": 21,
+    "category": "ART",
+    "title": "Paint, draw, or sculpt."
+  },
+  {
+    "id": 22,
+    "category": "ART",
+    "title": "\nLearn art history."
+  },
+  {
+    "id": 23,
+    "category": "ART",
+    "title": "Stay current on the latest fashions."
+  },
+  {
+    "id": 24,
+    "category": "ART",
+    "title": "Design a cake."
+  },
+  {
+    "id": 25,
+    "category": "ART",
+    "title": "Design a building or vehicle."
+  },
+  {
+    "id": 26,
+    "category": "ART",
+    "title": "Perform in front of others."
+  },
+  {
+    "id": 27,
+    "category": "ART",
+    "title": "Animate graphics."
+  },
+  {
+    "id": 28,
+    "category": "ART",
+    "title": "Design advertisements."
+  },
+  {
+    "id": 29,
+    "category": "ART",
+    "title": "Create works of art for others to enjoy."
+  },
+  {
+    "id": 30,
+    "category": "ART",
+    "title": "Learn music composition."
+  },
+  {
+    "id": 31,
+    "category": "ART",
+    "title": "Repair fine works of art."
+  },
+  {
+    "id": 32,
+    "category": "ART",
+    "title": "Learn how to style hair."
+  },
+  {
+    "id": 33,
+    "category": "ART",
+    "title": "Cook delicious meals for others."
+  },
+  {
+    "id": 34,
+    "category": "ART",
+    "title": "Learn how to design blueprints on a computer."
+  },
+  {
+    "id": 35,
+    "category": "ART",
+    "title": "Act in a movie or tv show."
+  },
+  {
+    "id": 36,
+    "category": "ART",
+    "title": "Edit videos."
+  },
+  {
+    "id": 37,
+    "category": "ART",
+    "title": "Design graphics for video games."
+  },
+  {
+    "id": 38,
+    "category": "IT",
+    "title": "Solve math problems."
+  },
+  {
+    "id": 39,
+    "category": "IT",
+    "title": "Start or run a business."
+  },
+  {
+    "id": 40,
+    "category": "IT",
+    "title": "Learn computer software programs."
+  },
+  {
+    "id": 41,
+    "category": "IT",
+    "title": "Use computer software to design art."
+  },
+  {
+    "id": 42,
+    "category": "IT",
+    "title": "Learn the legal system."
+  },
+  {
+    "id": 43,
+    "category": "IT",
+    "title": "Serve the community and keep it safe."
+  },
+  {
+    "id": 44,
+    "category": "IT",
+    "title": "Critique art, music, or performances."
+  },
+  {
+    "id": 45,
+    "category": "IT",
+    "title": "Bake or design cakes."
+  },
+  {
+    "id": 46,
+    "category": "IT",
+    "title": "Teach people new skills."
+  },
+  {
+    "id": 47,
+    "category": "IT",
+    "title": "Write a blog, magazine article, or novel."
+  },
+  {
+    "id": 48,
+    "category": "IT",
+    "title": "Observe human behavior."
+  },
+  {
+    "id": 49,
+    "category": "IT",
+    "title": "Discover why chemicals react to one another."
+  },
+  {
+    "id": 50,
+    "category": "IT",
+    "title": "Use math, science, and technology."
+  },
+  {
+    "id": 51,
+    "category": "IT",
+    "title": "\nFix, build, or assemble various items."
+  },
+  {
+    "id": 52,
+    "category": "IT",
+    "title": "Take care of people"
+  },
+  {
+    "id": 53,
+    "category": "IT",
+    "title": "Learn statistics and accounting."
+  },
+  {
+    "id": 54,
+    "category": "TECHNICIAN",
+    "title": "Use my artistic abilities."
+  },
+  {
+    "id": 55,
+    "category": "TECHNICIAN",
+    "title": "Code a website"
+  },
+  {
+    "id": 56,
+    "category": "TECHNICIAN",
+    "title": "Review data and present your findings."
+  },
+  {
+    "id": 57,
+    "category": "TECHNICIAN",
+    "title": "Respond to a cyber attack."
+  },
+  {
+    "id": 58,
+    "category": "TECHNICIAN",
+    "title": "Set up computers and printers for a business."
+  },
+  {
+    "id": 59,
+    "category": "TECHNICIAN",
+    "title": "Learn how computer hardware and software works."
+  },
+  {
+    "id": 60,
+    "category": "TECHNICIAN",
+    "title": "Help people with their technology issues."
+  },
+  {
+    "id": 61,
+    "category": "TECHNICIAN",
+    "title": "Create graphics."
+  },
+  {
+    "id": 62,
+    "category": "TECHNICIAN",
+    "title": "Learn multiple programming languages."
+  },
+  {
+    "id": 63,
+    "category": "TECHNICIAN",
+    "title": "Manage technology projects"
+  },
+  {
+    "id": 64,
+    "category": "TECHNICIAN",
+    "title": "Keep a business’ computer systems safe and secure."
+  },
+  {
+    "id": 65,
+    "category": "TECHNICIAN",
+    "title": "Ensure a businesses computer networks are working effectively."
+  },
+  {
+    "id": 66,
+    "category": "TECHNICIAN",
+    "title": "Learn how computers and servers function."
+  },
+  {
+    "id": 67,
+    "category": "TECHNICIAN",
+    "title": "Fix and upgrade computers."
+  },
+  {
+    "id": 68,
+    "category": "TECHNICIAN",
+    "title": "Design a website"
+  },
+  {
+    "id": 69,
+    "category": "TECHNICIAN",
+    "title": "Troubleshoot software code."
+  }
+]
